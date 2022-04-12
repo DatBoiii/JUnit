a604b2254dbb0fd4d5a27c97dbd71d329dfe70df
DatBoiii <90166694+DatBoiii@users.noreply.github.com>
2022-04-11 22:05:08 -0400

    first commit

---
+++ src/main/java/com/example/testingweb/WebLayerTest.java
@@ -0,0 +1,18 @@
+package com.example.testingweb;
+
+import java.beans.Transient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebMvcTest
+public class WebLayerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
+	}
+}
